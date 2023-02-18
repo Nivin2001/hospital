2613c6bef225c43a65d60df493785bad19f48be3
nivin shabat <nivin20012001@gmail.com>
2023-02-18 11:55:14 +0300

    hospitl

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WelcomeController extends Controller
+{
+    public function welcome (){
+        return view('Welcome');
+ 
+
+   }
+   public function create(){
+
+   }
+
+   public function index(){
+
+   }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
-
+use App\Http\Controllers\HospitalController;
+use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,49 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('admin.dashboard');
 });
+
+Route::prefix('admin') ->group(function(){
+Route::get('/Welcome','App/Http/Controllers/WelcomeController@welcome');
+
+Route::get('/Welcome',[WelcomeController::class,'welcome']);
+
+// Route::get('/hospital/index',[HospitalController::class,'index']->name('hospital.index'));
+
+Route::get('/hospital/create ',[HospitalController::class,'create']);
+
+Route::post('/hospital/store ',[HospitalController::class,'store']);
+
+Route::get('/hospital/show/{id} ',[HospitalController::class,'show']);
+
+Route::get('/hospital/edit /{id}',[HospitalController::class,'edit']);
+
+Route::post('/hospital/update/{id} ',[HospitalController::class,'update ']);
+
+Route::delete('/hospital/destroy/{id} ',[HospitalController::class,'destroy']);
+});
+
+Route::fallback(function(){
+return view('error404');
+});
+
+
+
+// Route::resource('hospitals',HospitalController::class);
+
+
+
+
+
+
+
+// اول بايراميتر هو المسار
+// ثاني باراميتر هو controller
+//  controller بستدعيه باسم 
+// بعدها @ 
+// اسم الدالة  الي انشاتها   في view 
+
+// Route::post();
+// Route::delete();
+
+
 
